f266a2752247b83911f6fb0135f3adcf502052f0
Theynvyjeffrey <jomoakah3@gmail.com>
2024-03-11 14:04:50 +0100

    defined the slot machince function

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ symbol_count = {
     "G": 4,
     "D": 8
 }
+def get_slot_machine_spin(rows,cols,symbols):
+    all_symbols = []
+    for symbols, symbol_count in symbols.items:
+        for _ in range(symbol_count):
+            all_symbols.append(symbols)
 
+    columns = [[],[],[]]
+    
+
+    
 def deposit():
     while True:
         amount = input("Enter the amount you want to deposit?: $")
