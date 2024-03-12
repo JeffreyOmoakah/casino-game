554870d347c7b28dfb716c2ae042c059a69890bc
Theynvyjeffrey <jomoakah3@gmail.com>
2024-03-12 16:59:23 +0100

    defined the print slot machine function

--- main.py
+++ main.py
@@ -24,13 +24,21 @@ def get_slot_machine_spin(rows,cols,symbols):
         columns = []
         current_symbols = all_symbols[:]
         for _ in range(rows):
-            value = random.choice(current_symbols)
+            value = random.choice(current_symbols) 
             current_symbols.remove(value)
             column.append(value)
 
         columns.append(columns)
 
     return columns
+
+def print_slot_machine(columns):
+    for row in range(len(column[0])):
+        for i, column in enumerate(columns):
+            if i != len(column) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
 
     
 def deposit():
