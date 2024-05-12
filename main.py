b1022ebc214037f5ccb525785c715fc514a764d5
Theynvyjeffrey <jomoakah3@gmail.com>
2024-05-12 14:34:11 +0100

    improve the code

--- main.py
+++ main.py
@@ -14,13 +14,6 @@ symbol_count = {
     "D": 8
 }
 
-symbol_value = {
-    "A": 2,
-    "B": 6,
-    "G": 4,
-    "D": 8
-}
-
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
